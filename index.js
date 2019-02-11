d0b95ba6ff58737542b61800b936d328e4404f7e
Jamie Hall <hall.jamie@gmail.com>
2019-02-11 18:09:03 -0500

    Endpoints in place. Some error checking completed

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // implement your API here
+
+// https://github.com/JamieHall1962/node-express-mini/pull/1
+
+
 const express = require("express");
 
 const db = require("./data/db.js");
@@ -17,18 +21,22 @@ server.get("/api/users", (req, res) => {
       res.status(200).json(users);
     })
     .catch(err => {
-      res.status(err.code).json({ success: false, message: err.message });
+      res.status(500).json({error: "The users information could not be retrieved." });
     });
 });
 
 server.get("/api/users/:id", (req, res) => {
     userId=req.params.id;
     db.findById(userId)
-      .then(users => {
-        res.status(200).json(users);
-      })
+      .then(user => {
+          if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({ message: "The user with the specified ID does not exist." }); 
+      }
+    }) 
       .catch(err => {
-        res.status(err.code).json({ success: false, message: err.message });
+        res.status(500).json({ error: "The user information could not be retrieved." })
       });
   });
 
